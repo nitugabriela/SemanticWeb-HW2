12898087ed869f39801d4ea93c5c4eeb2fe16b0a
Ela <gabrielanitu34@yahoo.com>
2026-05-18 10:17:12 +0300

    Initial project setup and RDF/XML file for ex 1

---
+++ src/main/java/com/hw2/book_recommender/BookRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.hw2.book_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hw2/book_recommender/BookRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw2.book_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
